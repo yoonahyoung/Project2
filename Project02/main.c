074335ab0a9fc9c74652a29765de07f10f54d818
yoonahyoung <97347193+yoonahyoung@users.noreply.github.com>
2022-02-05 18:11:01 +0900

    Create main.c

---
+++ Project02/main.c
@@ -0,0 +1,45 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	short a = 1;
+	long b = 2;
+	int c = 3;
+
+	unsigned short d = 4;
+	unsigned long e = 5;
+	unsigned int f = 6;
+
+	cout << "a = " << a << endl;
+	cout << "b = " << b << endl;
+	cout << "c = " << c << endl;
+	cout << "d = " << d << endl;
+	cout << "e = " << e << endl;
+	cout << "f = " << f << endl;
+
+	float g = 7;
+	double h = 8;
+	
+	cout << "g = " << g << endl;
+	cout << "h = " << h << endl;
+
+	char A = 48;
+	char AA = 'A';
+	cout << "A = " << A << endl;
+	cout << "AA = " << AA << endl;
+
+	bool TRUE = true;
+	bool FALSE = false;
+
+	cout << "TRUE = " << TRUE << endl;
+	cout << "FALSE = " << FALSE << endl;
+
+
+
+
+
+
+
+	return 0;
+}
