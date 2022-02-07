6e11e4235b9be0303ed8b92b463ab6631777f685
yoonahyoung <97347193+yoonahyoung@users.noreply.github.com>
2022-02-07 20:24:49 +0900

    Create main.c

---
+++ Project03/main.c
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	//11111111 11111111
+	unsigned short a = -1;
+
+	cout << "a = " << a << endl;
+
+	short b = 65535;
+
+	cout << "b = " << b << endl;
+
+	return 0;
+}
